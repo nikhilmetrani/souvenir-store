c42dd899c0c112e2192e5ce865b58130b8e3a273
NikhilMetrani <nikhilmetrani@live.com>
2015-03-13 21:35:43 +0800

    Move eclipse project to SouvenirStore folder and update class path accordingly. added junit module to dependencies to allow unit test execution

---
+++ SouvenirStore/src/sg/edu/nus/iss/se23pt2/pos/StoreApplication.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package sg.edu.nus.iss.se23pt2.pos;
+
+/**
+ * @author Nikhil Metrani
+ *
+ */
+public class StoreApplication {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("StoreApplication main executed");
+		return;
+	}
+
+}
