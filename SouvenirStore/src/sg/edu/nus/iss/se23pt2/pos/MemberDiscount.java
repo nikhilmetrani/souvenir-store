1e343b2b0ccc150598b199b15bbd272b1cb7b2c2
debasish <debasish@a0135916@u.nus.edu>
2015-03-19 02:01:14 +0800

    modified the return type of discountPercentage attribute:MemberDiscount

--- SouvenirStore/src/sg/edu/nus/iss/se23pt2/pos/MemberDiscount.java
+++ SouvenirStore/src/sg/edu/nus/iss/se23pt2/pos/MemberDiscount.java
@@ -2,7 +2,7 @@ package sg.edu.nus.iss.se23pt2.pos;
 
 public class MemberDiscount extends Discount{
 
-    public MemberDiscount(String discountCode, String description, String startDate, String periodInDays, Integer discountPercentage, String applicableTo)
+    public MemberDiscount(String discountCode, String description, String startDate, String periodInDays, double discountPercentage, String applicableTo)
     {
         super(discountCode, description, startDate, periodInDays, discountPercentage, applicableTo);
     }
