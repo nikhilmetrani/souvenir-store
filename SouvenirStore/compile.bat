13ddedd0464d5ef03696c3e3ab6705f7ef27241e
JV <A0135863@u.nus.edu>
2015-03-23 17:11:01 +0800

    - To set correct path in setenv.bat - To pause the screen after compilation to view the output

--- SouvenirStore/compile.bat
+++ SouvenirStore/compile.bat
@@ -5,3 +5,4 @@ call setenv.bat
 if not exist bin md bin
 echo Attempting to compile sg.edu.nus.iss.se23pt2.pos.StoreApplication
 javac -d bin -sourcepath src -cp src\sg\edu\nus\iss\se23pt2\pos\*.java
+pause
--- SouvenirStore/setenv.bat
+++ SouvenirStore/setenv.bat
@@ -1,2 +1,2 @@
 set JAVA_HOME="C:\Program Files\Java\jdk1.8.0_31"
-set PATH=%PATH%;%JAVA_HOME%\bin
+set PATH=%JAVA_HOME%\bin
