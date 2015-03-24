f7b8eb680b3f42e4d1a412fe61192079dd61d8d2
JV <A0135863@u.nus.edu>
2015-03-24 12:20:14 +0800

    - Changed "bin" to "classes" as per requirement

--- SouvenirStore/run.bat
+++ SouvenirStore/run.bat
@@ -3,4 +3,4 @@ cd /d %~dp0
 echo Setting up environment...
 call setenv.bat
 echo Attempting to run sg.edu.nus.iss.se23pt2.pos.StoreApplication
-java -classpath bin sg.edu.nus.iss.se23pt2.pos.StoreApplication %*
+java -classpath classes sg.edu.nus.iss.se23pt2.pos.StoreApplication %*
