b095a7dff4cf6caba6be7185bb57f94209d95381
JV <A0135863@u.nus.edu>
2015-03-24 12:16:08 +0800

    - To add libraries for TDD - To compile TDD classes - Changed bin folder to classes as per requirement

--- SouvenirStore/compile.bat
+++ SouvenirStore/compile.bat
@@ -2,7 +2,8 @@
 cd /d %~dp0
 echo Setting up environment...
 call setenv.bat
-if not exist bin md bin
-echo Attempting to compile sg.edu.nus.iss.se23pt2.pos.StoreApplication
-javac -d bin -sourcepath src -cp src\sg\edu\nus\iss\se23pt2\pos\*.java
+if not exist classes md classes
+echo Attempting to compile SouvenirStore Application
+javac -d classes -sourcepath src -cp src\sg\edu\nus\iss\se23pt2\pos\*.java 
+javac -d classes -sourcepath test -cp lib\hamcrest-core-1.3.jar;lib\junit-4.12.jar;classes;test; test\sg\edu\nus\iss\se23pt2\pos\*.java test\sg\edu\nus\iss\se23pt2\pos\datastore\*.java
 pause
