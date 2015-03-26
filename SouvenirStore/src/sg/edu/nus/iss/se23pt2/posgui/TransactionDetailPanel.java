2bee8309947b1d8a99a8b7fe7c34fb09e4c13186
Rishi <a0135914@u.nus.edu>
2015-03-26 23:22:47 +0800

    Formatting done

--- SouvenirStore/src/sg/edu/nus/iss/se23pt2/posgui/TransactionDetailPanel.java
+++ SouvenirStore/src/sg/edu/nus/iss/se23pt2/posgui/TransactionDetailPanel.java
@@ -14,20 +14,6 @@ import javax.swing.border.BevelBorder;
 import sg.edu.nus.iss.se23pt2.pos.Item;
 import sg.edu.nus.iss.se23pt2.pos.Transaction;
 
-
-
-/**
-* This code was edited or generated using CloudGarden's Jigloo
-* SWT/Swing GUI Builder, which is free for non-commercial
-* use. If Jigloo is being used commercially (ie, by a corporation,
-* company or business for any purpose whatever) then you
-* should purchase a license for each developer using Jigloo.
-* Please visit www.cloudgarden.com for details.
-* Use of Jigloo implies acceptance of these licensing terms.
-* A COMMERCIAL LICENSE HAS NOT BEEN PURCHASED FOR
-* THIS MACHINE, SO JIGLOO OR THIS CODE CANNOT BE USED
-* LEGALLY FOR ANY CORPORATE OR COMMERCIAL PURPOSE.
-*/
 /**
  * 
  * @author Rushabh Shah 
@@ -54,8 +40,7 @@ public class TransactionDetailPanel extends JPanel {
 	
 	private void initGUI() {
 		try {
-			this.setPreferredSize(new java.awt.Dimension(650, 453
-					));
+			this.setPreferredSize(new java.awt.Dimension(650, 453));
 			this.setLayout(null);
 			{
 				transactionDetailLabel = new JLabel();
