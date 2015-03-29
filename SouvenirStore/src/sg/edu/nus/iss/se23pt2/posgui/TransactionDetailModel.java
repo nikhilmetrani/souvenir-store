47f9fa1c58bce039d249478c671de04b39b6960b
Rishi <a0135914@u.nus.edu>
2015-03-29 21:52:51 +0800

    Transaction detail model updated corresponding to the table view for transaction detail class.

--- SouvenirStore/src/sg/edu/nus/iss/se23pt2/posgui/TransactionDetailModel.java
+++ SouvenirStore/src/sg/edu/nus/iss/se23pt2/posgui/TransactionDetailModel.java
@@ -1,42 +1,65 @@
 package sg.edu.nus.iss.se23pt2.posgui;
 
+import javax.swing.table.AbstractTableModel;
+
+import sg.edu.nus.iss.se23pt2.pos.Transaction;
+
 /**
  * 
  * @author Rushabh Shah 
  *
  */
-public class TransactionDetailModel extends TransactionModel {
+public class TransactionDetailModel extends AbstractTableModel  {
+
+	private static final long serialVersionUID = 1L;
+
+	private Transaction transaction;
 	
-	private String productId;
+	private String[] columnNames;
+
+	private String[][] transDetailModel;
 	
-	private int quantity;
-
-	public String getProductId() {
-		return productId;
+	
+	public TransactionDetailModel(Transaction transaction){
+		this.transaction = transaction;
+		columnNames = new String[] { "Transaction Id", "Member Id","Date","Product Id","Quantity" };
+		transDetailModel=new String[0][0];
+			if (this.transaction != null  && transaction.getItems()!=null) {
+				transDetailModel = new String[getRowCount()][getColumnCount()];
+				for (int i = 0; i < this.transaction.getItems().size(); i++) {
+				int j = 0;
+					transDetailModel[i][j++] = String.valueOf(this.transaction.getId());
+					transDetailModel[i][j++] = this.transaction.getCustomer().getId();
+					transDetailModel[i][j++] = this.transaction.getDate();
+					transDetailModel[i][j++] = this.transaction.getItems().get(i).getProduct().getId();
+					transDetailModel[i][j++] = String.valueOf(this.transaction.getItems().get(i).getQuantity());
+				}
+			}
+		
 	}
-
-	public void setProductId(String productId) {
-		this.productId = productId;
-	}
-
-	public int getQuantity() {
-		return quantity;
-	}
-
-	public void setQuantity(int quantity) {
-		this.quantity = quantity;
+	
+	@Override
+	public int getColumnCount() {
+		return columnNames.length;
 	}
 
 	@Override
-	public String toString() {
-		return "Id= "
-				+ getTransactionId() + ", MemberId= " + getMemberId()
-				+ ", Date= " + getDate() + ", ProductId= " + productId
-				+ ", Qty= " + quantity;
+	public int getRowCount() {
+		if(transaction!=null && transaction.getItems()!=null){
+			return transaction.getItems().size();
+		}
+		else
+			return 0;
 	}
 
+	@Override
+	public Object getValueAt(int row, int col) {
+		return transDetailModel[row][col];
+	}
 	
-	
+	public String getColumnName(int col) {
+        return columnNames[col];
+    }
 	
 	
 
