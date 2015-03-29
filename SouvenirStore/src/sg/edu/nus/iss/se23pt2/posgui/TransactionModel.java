c329cd671211fe0e605749a6bc54e45ce779aa89
Rishi <a0135914@u.nus.edu>
2015-03-29 21:51:28 +0800

    Transaction model updated corresponding to the search table view

--- SouvenirStore/src/sg/edu/nus/iss/se23pt2/posgui/TransactionModel.java
+++ SouvenirStore/src/sg/edu/nus/iss/se23pt2/posgui/TransactionModel.java
@@ -1,45 +1,63 @@
 package sg.edu.nus.iss.se23pt2.posgui;
+
+import java.util.List;
+
+import javax.swing.table.AbstractTableModel;
+
+import sg.edu.nus.iss.se23pt2.pos.Transaction;
+
 /**
  * 
  * @author Rushabh Shah
  *
  */
-public class TransactionModel {
+public class TransactionModel extends AbstractTableModel {
+
+	private static final long serialVersionUID = 1L;
+
+	private List<Transaction> transactions;
 	
-	private int transactionId;
+	private String[] columnNames;
 	
-	private String date;
+	private String[][] transModel;	
 	
-	private String memberId;
-
-	public int getTransactionId() {
-		return transactionId;
-	}
-
-	public void setTransactionId(int transactionId) {
-		this.transactionId = transactionId;
-	}
-
-	public String getDate() {
-		return date;
-	}
-
-	public void setDate(String date) {
-		this.date = date;
-	}
-
-	public String getMemberId() {
-		return memberId;
-	}
-
-	public void setMemberId(String memberId) {
-		this.memberId = memberId;
+	
+	public TransactionModel(List<Transaction> transactions){
+	this.transactions = transactions;
+	columnNames = new String[] { "Transaction Id", "Member Id","Date" };
+	transModel=new String[0][0];
+		if (transactions != null) {
+			transModel = new String[getRowCount()][getColumnCount()];
+			for (int i = 0; i < transactions.size(); i++) {
+			int j = 0;
+				transModel[i][j++] = String.valueOf(transactions.get(i).getId());
+				transModel[i][j++] = transactions.get(i).getCustomer().getId();
+				transModel[i][j++] = transactions.get(i).getDate();
+			}
+		}
 	}
 
 	@Override
-	public String toString() {
-		return "TransactionId = " + transactionId + ",     Date="
-				+ date + ",     MemberId=" + memberId;
+	public int getColumnCount() {
+		return columnNames.length;
 	}
+
+	@Override
+	public int getRowCount() {
+		if(transactions!=null){
+			return transactions.size();
+		}
+		else
+			return 0;
+	}
+
+	@Override
+	public Object getValueAt(int row, int col) {
+		return transModel[row][col];
+	}
+	
+	public String getColumnName(int col) {
+        return columnNames[col];
+    }
 
 }
